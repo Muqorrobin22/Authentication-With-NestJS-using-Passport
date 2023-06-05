093b79a8ca2ac5dcafdce928a970bfd3f718abcb
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2023-06-06 01:57:59 +0700

    done creating base REST API (without JWT)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() request) {
     return request.user;
